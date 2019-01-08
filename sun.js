2c984309cb598e78094eb37af0107f0e83538308
Alvaro <alvaro.manera@fwd.cloud>
2019-01-08 23:24:21 +0100

    [sun.js] 2019.01.08-23.24.20

--- sun.js
+++ sun.js
@@ -1,6 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const SunCalc = require("suncalc");
 
+const background = "hsl(0, 0%, 98%)";
 const settings = {
   dimensions: [2048, 2048]
 };
@@ -35,7 +36,7 @@ const sketch = () => {
   };
 
   return ({ context, width, height }) => {
-    context.fillStyle = "hsl(0, 0%, 98%)";
+    context.fillStyle = background;
     context.fillRect(0, 0, width, height);
 
     context.save();
@@ -51,34 +52,69 @@ const sketch = () => {
       ); //madrid
 
       context.strokeStyle = "black";
-      context.lineWidth = 5;
+      context.lineWidth = 3;
 
       drawCurve(context, points); // add cardinal spline to path
+
+      const maxVal = (max, currentValue) =>
+        max > currentValue.y ? max : currentValue.y;
+
+      const minVal = (min, currentValue) =>
+        min < currentValue.y ? min : currentValue.y;
+
+      const max = points.reduce(maxVal);
+      const min = points.reduce(minVal);
+
+      // context.font = "50px serif";
+      // context.fillText("" + month, 500, 0);
       context.stroke();
-      context.translate(width / -29, height / 12);
-    }
-    context.restore();
 
-    context.save();
-    context.translate(width / 10, height / 12);
-    for (let month = 0; month < 12; month++) {
-      const day = new Date();
-      day.setMonth(month);
+      // context.globalCompositeOperation = "source-over";
 
-      const points = convertToCoordinates(
+      let gradient = context.createLinearGradient(0, min, 0, 0);
+
+      context.fillStyle = gradient;
+      // Add three color stops
+      gradient.addColorStop(0, `hsl(${(month % 9) * 15 + 210},50%,50%)`);
+      gradient.addColorStop(1, background);
+
+      context.fill();
+
+      const points2 = convertToCoordinates(
         getSunAltitudes(day, 60.17, 24),
         width,
         height
-      ); //madrid
+      ); //helsinki
 
       context.strokeStyle = "black";
       context.lineWidth = 1;
 
-      drawCurve(context, points); // add cardinal spline to path
-      context.stroke();
+      // drawCurve(context, points2); // add cardinal spline to path
+      // context.stroke();
       context.translate(width / -29, height / 12);
     }
     context.restore();
+
+    // context.save();
+    // context.translate(width / 10, height / 12);
+    // for (let month = 0; month < 12; month++) {
+    //   const day = new Date();
+    //   day.setMonth(month);
+
+    //   const points = convertToCoordinates(
+    //     getSunAltitudes(day, 60.17, 24),
+    //     width,
+    //     height
+    //   ); //madrid
+
+    //   context.strokeStyle = "black";
+    //   context.lineWidth = 1;
+
+    //   drawCurve(context, points); // add cardinal spline to path
+    //   context.stroke();
+    //   context.translate(width / -29, height / 12);
+    // }
+    // context.restore();
   };
 };
 function drawCurve(ctx, points) {
